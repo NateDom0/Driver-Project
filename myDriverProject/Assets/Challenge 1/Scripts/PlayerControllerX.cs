27feab5deef65ea860aa82931eb6c6802e34f0e3
NateDom0 <nathanielrd98@gmail.com>
2022-05-14 03:11:23 -0700

    added fix to challenge 1

--- myDriverProject/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ myDriverProject/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    private float speed = 15.0f;
     public float rotationSpeed;
     public float verticalInput;
 
@@ -15,15 +15,17 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update() //changed from FixedUpdate to Update
     {
-        // get the user's vertical input
+        // get the user's vertical input (up and down)
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        //transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right, Time.deltaTime * rotationSpeed * verticalInput);
+
     }
 }
